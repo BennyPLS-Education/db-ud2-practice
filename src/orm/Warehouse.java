c305fa14d5c0652e06fe217de969e0d595579f4a
Benny <peptoni0345@gmail.com>
2023-11-13 19:22:28 +0100

    Add initial data and user interaction functionality
    This commit introduces the complete set of initial data for regions, warehouses, producers, products, clients, and orders in a new DATA.sql file. This data will seed the inventory management application we are building. This commit also adds new functionality to the Orders.java file that allows users to add, update, delete, and view orders through our application. It likewise includes the corresponding SQL triggers, views, and procedures that handle these operations in the DDL.sql file.
    
    Additional project configuration files and initial user interface java files were also created as part of setting up the project. These include xml configuration files for the IDE and gitignore files for the project directory and the .idea directory. The .iml file was added for IntelliJ IDEA project module settings. Initializations for main application entry point(Main.java) and exception handling utility method(Exceptions.java) were also added.
    
    This commit is necessary so that we can simulate a useful real-world scenario in our application and ensure that the basic CRUD operations are working as expected.

---
+++ src/orm/Warehouse.java
@@ -0,0 +1,76 @@
+package orm;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import static utils.Exceptions.manageError;
+
+public record Warehouse(int id, String name, int region) {
+    public static Warehouse createFrom(ResultSet set) throws SQLException {
+        return new Warehouse(
+            set.getInt("id"),
+            set.getString("name"),
+            set.getInt("region")
+        );
+    }
+
+    public static Warehouse[] getAll() {
+        ArrayList<Warehouse> warehouses = new ArrayList<>();
+
+        try (var connection = DriverManager.getConnection(DBData.URL, DBData.USER, DBData.PASSWORD)) {
+            var set = connection.createStatement().executeQuery("SELECT * FROM warehouses");
+            while (set.next()) {
+                warehouses.add(Warehouse.createFrom(set));
+            }
+        } catch (SQLException e) {manageError(e);}
+
+        return warehouses.toArray(new Warehouse[0]);
+    }
+
+    public static Warehouse get(int id) {
+        Warehouse warehouse = null;
+
+        try (var connection = DriverManager.getConnection(DBData.URL, DBData.USER, DBData.PASSWORD)) {
+            var set = connection.createStatement().executeQuery("SELECT * FROM warehouses WHERE id = " + id);
+            if (set.next()) {
+                warehouse = Warehouse.createFrom(set);
+            }
+        } catch (SQLException e) {manageError(e);}
+
+        return warehouse;
+    }
+
+    public void insert() {
+        try (var connection = DriverManager.getConnection(DBData.URL, DBData.USER, DBData.PASSWORD)) {
+            var statement = connection.prepareStatement("INSERT INTO warehouses (name, region) VALUES (?, ?)");
+            statement.setString(1, name);
+            statement.setInt(2, region);
+            statement.executeUpdate();
+        } catch (SQLException e) {manageError(e);}
+    }
+
+    public void update() {
+        try (var connection = DriverManager.getConnection(DBData.URL, DBData.USER, DBData.PASSWORD)) {
+            var statement = connection.prepareStatement("UPDATE warehouses SET name = ?, region = ? WHERE id = ?");
+            statement.setString(1, name);
+            statement.setInt(2, region);
+            statement.setInt(3, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {manageError(e);}
+    }
+
+    public void delete() {
+        try (var connection = DriverManager.getConnection(DBData.URL, DBData.USER, DBData.PASSWORD)) {
+            var statement = connection.prepareStatement("DELETE FROM warehouses WHERE id = ?");
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {manageError(e);}
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s in region: %s", name, Region.get(region).name());
+    }
+}
